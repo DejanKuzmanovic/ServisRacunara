e1e410e549f2a05cc0371cbbf4706e757cf3e8d5
Dejan Kuzmanovic <dejan.kuzmanovic@yahoo.com>
2020-03-28 12:23:35 +0100

    Kreirana aplikacija pomocu Spring Initializr-a

---
+++ src/main/java/com/servisracunara/pmf/PmfApplication.java
@@ -0,0 +1,13 @@
+package com.servisracunara.pmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmfApplication.class, args);
+	}
+
+}
