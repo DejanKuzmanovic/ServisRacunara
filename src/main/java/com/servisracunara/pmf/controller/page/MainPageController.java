cbb8c14d7d800b70b264c14f11d33dc4504097db
Dejan Kuzmanovic <dejan.kuzmanovic@yahoo.com>
2020-03-28 14:46:02 +0100

    Integrisana bootstrap tema

--- src/main/java/com/servisracunara/pmf/controller/page/MainPageController.java
+++ src/main/java/com/servisracunara/pmf/controller/page/MainPageController.java
@@ -1,4 +1,19 @@
 package com.servisracunara.pmf.controller.page;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("page")
 public class MainPageController {
+
+    private static final String MAIN_PAGE_VIEW_NAME = "main-page";
+
+    @GetMapping(value = "/index")
+    public ModelAndView loginMessage(){
+        return new ModelAndView(MAIN_PAGE_VIEW_NAME);
+    }
+
 }
